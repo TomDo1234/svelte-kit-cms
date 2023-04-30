e2fa5e802edc8b89822676620c570ea22974f509
TomDo1234 <tomtheawesome123@gmail.com>
2023-04-30 16:19:33 +1000

    res

---
+++ types.d.ts
@@ -0,0 +1,36 @@
+type FieldBase = {
+    name: string,
+    id?: boolean,
+    unique?: boolean,
+}
+
+type FieldWithType = FieldBase & {
+    type: "Int" | "String" | "DateTime" | "Boolean" | "Float" | "Decimal" | "BigInt" | "Bytes",
+    isCreatedAt?: false,
+    isUpdatedAt?: false,
+}
+
+type FieldWithCreatedAt = FieldBase & {
+    isCreatedAt: true,
+    isUpdatedAt?: false,
+    type?: never,
+}
+
+type FieldWithUpdatedAt = FieldBase & {
+    isCreatedAt?: false,
+    isUpdatedAt: true,
+    type?: never,
+}
+
+type Field = FieldWithType | FieldWithCreatedAt | FieldWithUpdatedAt;
+  
+type Model = {
+    name: string,
+    fields: Field[]
+}
+
+type SiriusConfig = {
+    provider: string,
+    url: string,
+    models: Model[]
+}
